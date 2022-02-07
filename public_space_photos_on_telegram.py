b6e26c5c3ddaac82056f1b4613c5429b34b92241
PaSeRouS <paser96@mail.ru>
2022-02-07 14:35:12 +0300

    Add function public_photos()

--- public_space_photos_on_telegram.py
+++ public_space_photos_on_telegram.py
@@ -9,43 +9,33 @@ from fetch_nasa import fetch_nasa_photos, fetch_epic_photos
 from fetch_spacex import fetch_spacex_last_launch
 
 
-def public_photo_on_telegram():
-    telegram_token = os.getenv("TELEGRAM_TOKEN")
-    publication_delay = os.getenv('PUBLICATION_DELAY')
+def public_photos_to_telegram(telegram_token, publication_delay):
+    directories = [ "images", "nasa_photo", "epic_photo"]
 
-    if not publication_delay:
-        publication_delay = 86400
-
-    photos_spacex_for_telegram = listdir("images")
-    photos_nasa_for_telegram = listdir("nasa_photo")
-    photos_epic_for_telegram = listdir("epic_photo")
-  
     bot = telegram.Bot(token=telegram_token)
 
-    while True:
-        for photo in photos_spacex_for_telegram:
-            filepath = f'images/{photo}'
-            bot.sendPhoto(chat_id="@paser_space_photo", photo=open(filepath, 'rb'))
-            time.sleep(int(publication_delay))
+    for directory in directories:
+        public_photo_to_telegram(bot, directory, publication_delay)
 
-        for photo in photos_nasa_for_telegram:
-            filepath = f'nasa_photo/{photo}'
-            bot.sendPhoto(chat_id="@paser_space_photo", photo=open(filepath, 'rb'))
-            time.sleep(int(publication_delay))
 
-        for photo in photos_epic_for_telegram:
-            filepath = f'epic_photo/{photo}'
-            bot.sendPhoto(chat_id="@paser_space_photo", photo=open(filepath, 'rb'))
-            time.sleep(int(publication_delay))
+def public_photos(bot, directory, publication_delay):
+    photos = listdir(directory)
+
+    for photo in photos:
+        filepath = f'images/{photo}'
+        bot.sendPhoto(chat_id="@paser_space_photo", photo=open(filepath, 'rb'))
+        time.sleep(int(publication_delay))
 
 
 if __name__ == '__main__':
     load_dotenv()
 
     nasa_token = os.getenv("NASA_TOKEN")
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+    publication_delay = 86400 if not os.getenv('PUBLICATION_DELAY') else os.getenv('PUBLICATION_DELAY')
 
     fetch_spacex_last_launch()
     fetch_nasa_photos(nasa_token)
     fetch_epic_photos(nasa_token)
 
-    public_photo_on_telegram()
+    public_photos_to_telegram(telegram_token, publication_delay)
